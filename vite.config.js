e59f722a26c0690972092727f2ea81db63e9377d
Riya Bawane <riyabawane05@gmail.com>
2025-05-22 12:46:12 +0530

    asdfghjk

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    allowedHosts: ['riyabawane.site'], // <-- Add your domain here
+    host: '0.0.0.0', // Needed if you're serving from a VPS or external IP
+    port: 5173
+  }
 })
