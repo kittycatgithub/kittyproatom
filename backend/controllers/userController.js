469988b09c91ed6f7ffe9964fe2435ed2ad726d0
Riya Bawane <riyabawane05@gmail.com>
2025-06-12 14:07:08 +0530

    cvbnjk

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -90,7 +90,7 @@ export const isAuth = async (req, res) => {
 export const logout = async ( req, res ) => {
     try {
         res.clearCookie('token',{
-            httpOnly: true, 
+            // httpOnly: true, 
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         })
